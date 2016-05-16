8630fb7bab4f9d14a0e1b2ecfd0d6578b9ba8847
Arielle Mohr <ariellemohr@Arielles-MacBook-Pro-2.local>
2016-05-16 17:24:38 -0400

    testing shake

--- hi-fi/js/shake.js
+++ hi-fi/js/shake.js
@@ -23,7 +23,14 @@ if (typeof window.DeviceMotionEvent != 'undefined') {
         var change = Math.abs(x1-x2+y1-y2+z1-z2);
 
         if (change > sensitivity) {
-            window.location.href=Lightbox;
+            function showLightBox() 
+  
+    if (started) return;
+    started = setTimeout(function(){
+        Lightbox.start(document.getElementsByClassName('vlightbox1'));
+        started;
+    },500);
+  
         }
 
         // Update new position
