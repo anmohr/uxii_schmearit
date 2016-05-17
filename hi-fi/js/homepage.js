eb1d550fa2472e342dbc3b85df9aa50cf1318dcb
Arielle Mohr <ariellemohr@n2-155-226.dhcp.drexel.edu>
2016-05-17 18:09:37 -0400

    shake function

--- hi-fi/js/homepage.js
+++ hi-fi/js/homepage.js
@@ -29,3 +29,19 @@ if(localStorage.getItem('rewards') > 0){
     }
 
   });
+
+var myShakeEvent = new Shake({
+    threshold: 15, // optional shake strength threshold
+    timeout: 1000 // optional, determines the frequency of event generation
+});
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+//function to call when shake occurs
+function shakeEventDidOccur () {
+
+    //put your own code here etc.
+ $('#myModal').foundation('open');
+}
