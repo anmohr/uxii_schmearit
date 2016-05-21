49f14d86f30dd210fa78edbd579712106668c213
ebohan15 <ebohan15@gmail.com>
2016-05-21 19:55:03 -0400

    added basic local storage functionality for rewards

--- hi-fi/js/app.js
+++ hi-fi/js/app.js
@@ -13,3 +13,24 @@ localStorage.setItem("typeDescriptions", JSON.stringify(movies));
 // retrieving the data
 var retrievedData = localStorage.getItem("starRating");
 var retrievedData = localStorage.getItem("typedescriptions");
+
+//rewards start//
+function clickCounter() {
+
+    if (typeof (Storage) !== "undefined") {
+
+        if (localStorage.clickcount<10) {
+            
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+            document.getElementById("result").innerHTML = "You have " + localStorage.clickcount + " pionts(s).";
+        } else {
+
+            localStorage.clickcount = 1;
+            document.getElementById("result").innerHTML = "You have a free bagel " + localStorage.clickcount + " time(s).";
+        }
+        
+    } else {
+
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
