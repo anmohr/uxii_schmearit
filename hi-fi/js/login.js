b9362ba49adf1cfead18f57e353c5b9fdafc6994
Faye Teng <fayeteng@Fayes-MacBook-Pro.local>
2016-05-24 22:42:41 -0400

    local storage

--- hi-fi/js/login.js
+++ hi-fi/js/login.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
 	homepagePanel();
     
     var nameSpace = $(".user-name");
-	var emailSave = $(".user-info-save");
+	var nameSave  = $(".user-name-save");
 
 	// var element = $("#id");
 
 	submitbutton.click(function(event) {
 		event.preventDefault();
+        
         localStorage.setItem("user-name", $(".user-name-input").val());
 		localStorage.setItem("user-email", $(".user-email").val());
 		localStorage.setItem("user-password", $(".user-password").val());
@@ -19,7 +20,7 @@ $(document).ready(function(){
 		//nameSpace.html(localStorage.getItem("user-name"));
 
 
-		$('.user-info').html(localStorage.getItem("user-name" + localStorage.getItem("user-email") + localStorage.getItem("user-password"));
+	   $('.user-info').html(localStorage.getItem("user-name") + localStorage.getItem("user-email") + localStorage.getItem("user-password"));
 	}
 
 	//// Clear localStorage
@@ -32,10 +33,10 @@ $(document).ready(function(){
 $('.login-submit').click(function(event) {
 	event.preventDefault();
 
-	var useremail = localStorage.getItem("user-email");
+	var useremail = localStorage.getItem("user-name");
 
 	if( $('.login-email').val() == useremail ) {
-		alert("Thanks for creating an account!");
+		alert("right email");
 		localStorage.setItem("login-status", "true");
 		console.log(localStorage.getItem('login-status'));
 	} else {
