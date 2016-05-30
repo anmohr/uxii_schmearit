765894444d1528cc6f200efc5428a6851ce77421
Xin Yi Wang <xinyi@Xins-MacBook-Pro.local>
2016-05-30 14:32:56 -0400

    setup of ratings local storage

---
+++ launch/js/ratings.js
@@ -0,0 +1,15 @@
+// set up a star rating array
+var rates = [1, 2, 3, 4, 5];
+
+
+// set up a description array
+var descriptions = ["sweet", "savory", "unusual", "other"];
+
+
+// storing the array as a string
+//localStorage.setItem("starRating", JSON.stringify(movies));
+//localStorage.setItem("typeDescriptions", JSON.stringify(movies));
+
+// retrieving the data
+var retrievedData = localStorage.getItem("starRating");
+var retrievedData = localStorage.getItem("typedescriptions");
