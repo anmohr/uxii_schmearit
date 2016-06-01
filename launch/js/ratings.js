c53da178d8f97831122328c1a152a2f1864f1acd
Xin Yi Wang <xinyi@Xins-MacBook-Pro.local>
2016-06-01 00:43:30 -0400

    added code for saving local storage from ratings page, pulling ls and printing onto popular bagels page

--- launch/js/ratings.js
+++ launch/js/ratings.js
@@ -14,63 +14,32 @@
 //var retrievedData = localStorage.getItem("starRating");
 //var retrievedData = localStorage.getItem("typedescriptions");
 
+$('.sub_button').click(function(e){
+    console.log(localStorage.getItem("score"));
+    console.log(localStorage.getItem("category"));
+    console.log(localStorage.getItem("item-name"));
+    
+    //Write to the thing
+    
+});
+
+$('.radio_star').click(function(e){
+    localStorage.setItem("score", e.currentTarget.value);
+});
+
+$('.button_cate').click(function(e){
+    localStorage.setItem("category", e.currentTarget.getAttribute("value"));
+});
+
+$('.rateMe-button').click(function(e){
+    
+    var itemName = $('.menu-item').data('menu-item');
+    localStorage.setItem("item-name", itemName);
+    
+});
 
 
 $(document).ready(function(){
-	var menuItems = $(".menu-item");
-	var LSCart;
-	totalRow = $('.total-row');
-    
-    var itemCategory;
-    var itemDescription;
-    var itemPrice;
-    var itemHeart;
-    var itemStar;
-    //var ratings = [itemName, itemDescription, itemPrice, itemHeart, itemStar];
-
-    
-    // ratings structure
-    // -item
-    // --name 0	
-    // --description 1
-    // --price 2
-    // --heart 3	
-    // --star 4	
-
-
-	menuItems.click(function() {
-		var itemName = $(this).data('menu-item');
-		var itemPrice = $(this).data('menu-price');
-		//clearing quantity for that item
-		var itemQuantity = 1;
-		//clearing the cart variable and setting it as an array
-		LSCart = [];
-
-		if(localStorage.getItem("cart")){
-			//if cart exists already, set value of lscart to localstorage
-			LSCart = JSON.parse(localStorage.getItem("cart"));
-		}
-// look through LSCart and see if this item already exists
-		for(var i = 0; i < LSCart.length; i++){
-			if(LSCart[i][0] == itemName){
-				itemQuantity += LSCart[i][2];
-				LSCart.splice(i, 1);
-			}// create array of item we just clicked
-		}
-
-		var item = [itemName, itemPrice, itemQuantity];
-		//now we are adding it to the array LScart
-
-		LSCart.push(item);
-
-		localStorage.setItem("cart", JSON.stringify(LSCart));
-
-		populateCart(LSCart);
-
-	});
-
-	if(localStorage.getItem("cart")){
-		LSCart = JSON.parse(localStorage.getItem("cart"));
-		populateCart(LSCart);
-	}
+    var fill = document.getElementsByClassName('ls-item')[0];
+    fill.innerHTML = localStorage.getItem('item-name');
 });
